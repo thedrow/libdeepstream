8e4c7e6e6d9b4cc7b19c9b1e03c6ec5ffd6b43cb
Omer Katz <omer.drow@gmail.com>
2016-02-22 18:23:56 +0200

    Escape all the things.

--- ci-install-cmake.sh
+++ ci-install-cmake.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-mkdir -p $HOME/.local/
-if [ $TRAVIS_OS_NAME == 'linux' ] ; then
+mkdir -p "$HOME/.local/"
+if [ "$TRAVIS_OS_NAME" = 'linux' ] ; then
   [ ! -e "$HOME/.local/bin/cmake" ] && curl -L https://cmake.org/files/v3.4/cmake-3.4.3-Linux-x86_64.sh | sh -s -- --prefix="$HOME/.local/" --skip-license
-elif [ $TRAVIS_OS_NAME == 'osx' ]; then
+elif [ "$TRAVIS_OS_NAME" = 'osx' ]; then
   brew update
   brew upgrade cmake --cleanup
 fi
