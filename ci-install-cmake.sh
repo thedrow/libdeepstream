b57301613e932ae5c28e05233bcad0b857327f04
Omer Katz <omer.drow@gmail.com>
2016-02-22 18:16:52 +0200

    Don't fail if .local already exists.

--- ci-install-cmake.sh
+++ ci-install-cmake.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-mkdir $HOME/.local/
+mkdir -p $HOME/.local/
 if [ $TRAVIS_OS_NAME == 'linux' ] ; then
   [ ! -e "$HOME/.local/bin/cmake" ] && curl -L https://cmake.org/files/v3.4/cmake-3.4.3-Linux-x86_64.sh | sh -s -- --prefix="$HOME/.local/" --skip-license
 elif [ $TRAVIS_OS_NAME == 'osx' ]; then
