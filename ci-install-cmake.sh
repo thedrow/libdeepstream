c6be5a3e7491907b18da1fa8aeba505cf12a53de
Omer Katz <omer.drow@gmail.com>
2016-02-22 19:28:09 +0200

    Revert "Install cmake on osx without updating homebrew."
    This reverts commit a40b2f0815ff0aa1e7e354a90d73f1a3e98bd160.

--- ci-install-cmake.sh
+++ ci-install-cmake.sh
@@ -6,5 +6,6 @@ if [ "$TRAVIS_OS_NAME" = 'linux' ] ; then
     sh cmake-3.4.3-Linux-x86_64.sh --prefix="$HOME/.local/" --skip-license
   fi
 elif [ "$TRAVIS_OS_NAME" = 'osx' ]; then
-  brew reinstall https://raw.githubusercontent.com/Homebrew/homebrew/master/Library/Formula/cmake.rb
+  brew update
+  brew upgrade cmake --cleanup
 fi
