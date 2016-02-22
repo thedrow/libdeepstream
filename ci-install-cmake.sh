4c299c151966b8382690f2e77968afe93655bb09
Omer Katz <omer.drow@gmail.com>
2016-02-22 18:12:24 +0200

    Portable script to install latest cmake that actually works.

---
+++ ci-install-cmake.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+mkdir $HOME/.local/
+if [ $TRAVIS_OS_NAME == 'linux' ] ; then
+  [ ! -e "$HOME/.local/bin/cmake" ] && curl -L https://cmake.org/files/v3.4/cmake-3.4.3-Linux-x86_64.sh | sh -s -- --prefix="$HOME/.local/" --skip-license
+elif [ $TRAVIS_OS_NAME == 'osx' ]; then
+  brew update
+  brew upgrade cmake --cleanup
+fi
