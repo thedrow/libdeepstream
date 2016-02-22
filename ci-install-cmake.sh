4a850b03f9cd34ac80ab963f84a6ca8501bf7c4c
Omer Katz <omer.drow@gmail.com>
2016-02-22 18:35:31 +0200

    No idea why piping doesn't work but this should.

--- ci-install-cmake.sh
+++ ci-install-cmake.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 mkdir -p "$HOME/.local/"
 if [ "$TRAVIS_OS_NAME" = 'linux' ] ; then
-  [ ! -e "$HOME/.local/bin/cmake" ] && curl -L https://cmake.org/files/v3.4/cmake-3.4.3-Linux-x86_64.sh | sh -s -- --prefix="$HOME/.local/" --skip-license
+  if [ ! -e "$HOME/.local/bin/cmake" ]; then
+    curl -O https://cmake.org/files/v3.4/cmake-3.4.3-Linux-x86_64.sh
+    sh cmake-3.4.3-Linux-x86_64.sh --prefix="$HOME/.local/" --skip-license
+  fi
 elif [ "$TRAVIS_OS_NAME" = 'osx' ]; then
   brew update
   brew upgrade cmake --cleanup
